70f2ddef737b1625cf89d1ddb363d28b30fd73e9
Elwes Honorato <elweshonorato@gmail.com>
2026-03-11 12:44:44 +0300

    chore(worker-chunk-text): annotate settings bundle type

--- domains/worker_chunk_text/src/app.py
+++ domains/worker_chunk_text/src/app.py
@@ -2,7 +2,7 @@
 
 from contracts.contracts import ChunkTextWorkerConfigContract
 from registry import DataHubPipelineJobs, GovernedRagJobId
-from pipeline_common.settings import SettingsProvider, SettingsRequest
+from pipeline_common.settings import SettingsBundle, SettingsProvider, SettingsRequest
 from pipeline_common.startup import RuntimeContextFactory, WorkerRuntimeLauncher
 from pipeline_common.startup.runtime_context import WorkerRuntimeContext
 from services.worker_chunk_text_service import WorkerChunkTextService
@@ -12,7 +12,7 @@ from startup.service_factory import ChunkTextServiceFactory
 
 def run() -> None:
     """Start chunk_text worker."""
-    settings = SettingsProvider(SettingsRequest(datahub=True, storage=True, queue=True)).bundle
+    settings: SettingsBundle = SettingsProvider(SettingsRequest(datahub=True, storage=True, queue=True)).bundle
     runtime_factory = RuntimeContextFactory(
         data_job_key=DataHubPipelineJobs.CUSTOM_GOVERNED_RAG.job(GovernedRagJobId.WORKER_CHUNK_TEXT),
         settings_bundle=settings,
